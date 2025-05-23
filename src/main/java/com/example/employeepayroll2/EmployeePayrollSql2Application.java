df13470d8881db7d4c60eee6e2cdb6ea3ea6d024
Sri Krishna Aditya Garimella <gskaditya007@gmail.com>
2025-05-23 15:32:43 +0530

    Welcome to Employee Payroll Application

---
+++ src/main/java/com/example/employeepayroll2/EmployeePayrollSql2Application.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSql2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSql2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeepayroll2/EmployeePayrollSql2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSql2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
