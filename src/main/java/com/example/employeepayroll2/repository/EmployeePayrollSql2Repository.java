5b09dd2e2ed8c4d6393b3a492476b9a943c11ac5
Sri Krishna Aditya Garimella <gskaditya007@gmail.com>
2025-05-23 17:41:30 +0530

    UC3: Implement Data Saving Functionality in Payroll Service Layer

---
+++ src/main/java/com/example/employeepayroll2/repository/EmployeePayrollSql2Repository.java
@@ -0,0 +1,7 @@
+package com.example.employeepayroll2.repository;
+
+import com.example.employeepayroll2.model.EmployeePayrollSql2Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeePayrollSql2Repository extends JpaRepository<EmployeePayrollSql2Model, Integer> {
+}
